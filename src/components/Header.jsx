04b4860662c4c6ddd133045aba9fadc90978079d
Segey Kostin <sergeyvladimirovish@gmail.com>
2022-03-29 15:21:47 +0300

    creater header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
+import "../styles/Header.scss";
+
 const Header = () => {
   return (
     <div>
-      <img srs="../images/logo.svg" alt="Audit logo" />
+      <img srs="logo.svg" alt="Audit logo" />
+      <nav className="header-nav-bar">
+        <div className="header-nav-bar-item left-side">
+          <button className="base-styled-btn" />
+          <button className="base-styled-btn" />
+          <button className="base-styled-btn" />
+        </div>
+        <div className="header-nav-bar-item right-side">
+          <button className="base-styled-btn" />
+          <button className="theme-change-btn" />
+        </div>
+      </nav>
     </div>
   );
 };
