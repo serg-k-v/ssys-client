d430b02ab89c5774ea6b16d80c759f4bc366145f
Segey Kostin <sergeyvladimirovish@gmail.com>
2022-03-29 14:24:38 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <img srs="../images/logo.svg" alt="Audit logo" />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Audit.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+const Audit = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Audit;
